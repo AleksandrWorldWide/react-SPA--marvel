57d832f965075576a653a7596e56ed0d3a4864de
Aleksandr <cifrotech@yandex.ru>
2022-01-23 17:34:55 +1000

    dynamic routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,10 @@
 
+
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 
-import { MainPage, ComicsPage } from "../pages";
+import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
 
 
 
@@ -21,6 +22,8 @@ const App = () => {
 					<Routes>
 						<Route path="/" element={<MainPage/>}/>
 						<Route path="/comics" element={<ComicsPage/>}/>
+						<Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+						<Route path="*" element={<Page404/>}/>
 					</Routes>
 				</main>
 			</div>
